2cea305b433ab6a438f5655a867dcd2c4cca06f8
akjeres <akjeres@gmail.com>
2020-04-04 21:39:57 +0300

    Initialize project

---
+++ app/js/common.js
@@ -0,0 +1,27 @@
+// NodeElement.prototype.closest() polyfill
+(function () {
+    if (!Element.prototype.matches) {
+        Element.prototype.matches = Element.prototype.matchesSelector ||
+            Element.prototype.webkitMatchesSelector ||
+            Element.prototype.mozMatchesSelector ||
+            Element.prototype.msMatchesSelector;
+    }
+})();
+(function () {
+    if (!Element.prototype.closest) {
+        Element.prototype.closest = function (css) {
+            var node = this;
+
+            while (node) {
+                if (node.matches(css)) return node;
+                else node = node.parentElement;
+            }
+            return null;
+        };
+    }
+
+})();
+// NodeElement.prototype.closest() polyfill ends
+window.addEventListener('load', () => {
+    console.log('loaded');
+});
---
+++ readme.md
@@ -0,0 +1,10 @@
+#Web courses application
+
+## Install dependencies
+npm i
+
+## Start project
+gulp
+
+## Tips
+If it is necessary, and gulp does not run normally, install gulp-cli via npm i gulp-cli
